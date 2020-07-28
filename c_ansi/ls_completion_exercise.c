0623bddd71015fbfa50d4ada5df821944c0e73a1
Stephen Zerfas <szerfas@lyft.com>
2020-07-28 10:10:24 -0700

    clean up MVP version of completion exercise

--- c_ansi/ls_completion_exercise.c
+++ c_ansi/ls_completion_exercise.c
@@ -3,27 +3,24 @@
 #include <dirent.h>
 #include <sys/stat.h>
 
+/* Approach to ls implementation
+If arguments, parse arguments for target directory
+Else, get cwd to use as target directory
+Open the target directory
+Traverse the target directory to get list of directory entries;
+For each directory entry, get the file_name and a piece of metadata, like it's size
+Print the file name and its size in a reasonable format
+*/
+
 /* TODO: handle arguments passed in */
 int main ()
 {
-//    #define MAXPATH 10000
-    /*
-    get cwd
-    open the cwd file
-    traverse the cwd file object to get list of file names stored within the file
-    */
     FILE *fp;
     DIR *dp;
     char *cwd;
     struct dirent *dir_entry;
     struct stat stbuf;
     char *file_name;
-//    char c;
-//    char cwd[MAXPATH];
-
-//    fp = fopen(cwd, "r");
-//    c = getc(fp);
-//    printf("%d\n", c);
 
     cwd = getcwd(NULL, 0);
     dp = opendir(cwd);
@@ -38,6 +35,8 @@ int main ()
         stat(dir_entry->d_name, &stbuf);
         printf("%30lld\n", stbuf.st_size);
     }
-
-//    printf("%s\n", getcwd(NULL, 0));
 }
+/* TODOs:
+- break out into a few functions
+- check if need to set function prototype and variable declarations separately after importing headers
+*/
